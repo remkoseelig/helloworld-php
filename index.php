72544031cf6c2d58d94b8dfa2c6b214663ec26b9
Remko Seelig <remko@tailsoft.nl>
2017-12-20 20:25:04 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Hello World </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+  <link rel="icon" type="image/png" href="images/favicon.png" />
+</head>
+<body>
+  <style>
+    #devon {
+      margin-top: 50px;
+      margin-bottom: 150px;
+    }
+    .col-centered {
+      float: none;
+      margin: 0 auto;
+    }
+  </style>
+  <div class="container-fluid">
+    <div id="devon" class="row">
+        <div class="col-centered">
+          <a href="https://devon.nl"><img src="images/devon.png"></a>
+        </div>
+    </div>
+    <div class="row">
+      <div class="col-centered">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Hello_world_c.svg/640px-Hello_world_c.svg.png">
+      </div>
+  </div>
+</body>
+</html>
